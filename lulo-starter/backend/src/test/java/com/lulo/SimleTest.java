0e147724aaeefa4e95b9715e7635ef108bde6911
Samuel Montealegre <montealegreq-samuel@javeriana.edu.co>
2026-04-07 14:43:26 -0500

    add first test

---
+++ lulo-starter/backend/src/test/java/com/lulo/SimleTest.java
@@ -0,0 +1,13 @@
+package com.lulo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+
+    @Test
+    void pruebaBasica() {
+        int resultado = 2 + 2;
+        assertEquals(4, resultado);
+    }
+}
